b1eaec6556edc29591e14de3c195010ceca9e695
Haeun Jeong <haeunjeong@Haeuns-MacBook-Pro.local>
2020-11-20 08:49:54 -0500

    added search function

--- src/app.jsx
+++ src/app.jsx
@@ -1,12 +1,29 @@
 import react, { useEffect, useState } from 'react';
 import { YOUTUBE_API_KEY } from './config/youtube.js';
-import './app.css';
+import styles from './app.module.css';
 import VideoList from './components/video_list/video_list.jsx';
+import SearchHeader from './components/search_header/search_header.jsx';
 
 function App() {
 	const [videos, setVideos] = useState([]);
+	const search = (query) => {
+		const requestOptions = {
+			method: 'GET',
+			redirect: 'follow',
+		};
 
-	//컴포넌트가 마운트 되었거나 업데이트 되었을 때 호출되는 것
+		fetch(
+			`https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${query}}&type=video&&key=${YOUTUBE_API_KEY}`,
+			requestOptions
+		)
+			.then((response) => response.json())
+			.then((result) =>
+				result.items.map((item) => ({ ...item, id: item.id.videoId }))
+			)
+			.then((items) => setVideos(items))
+			.catch((error) => console.log('error', error));
+	};
+
 	useEffect(() => {
 		const requestOptions = {
 			method: 'GET',
@@ -21,7 +38,12 @@ function App() {
 			.then((result) => setVideos(result.items))
 			.catch((error) => console.log('error', error));
 	}, []);
-	return <VideoList videos={videos} />;
+	return (
+		<div className={styles.app}>
+			<SearchHeader onSearch={search} />
+			<VideoList videos={videos} />;
+		</div>
+	);
 }
 
 export default App;
